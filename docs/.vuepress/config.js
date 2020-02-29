81306421e51cf7c38c17b9bd3c75e39f98f97490
xicanchen <1103015686@qq.com>
2020-02-29 15:03:16 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    title: '个人文档',
+    description: '练习文档'
+  }
+
+module.exports = {
+    themeConfig: {
+      sidebar: 'auto',
+      nav:[ // 导航栏配置
+        {text: '我们的优缺点', link: '/merits/' },
+        {text: '我能给你的未来', link: '/future/'},
+        {text: '我们的天意', link: '/fate/'},
+        {text: '微信公众号', link: 'https://baidu.com'}      
+      ],
+    }
+  }
+  
+
+
